001a4ab46778254e0dcb49fb50cbd3029e5bf273
Dahirabdi23 <86687537+Dahirabdi23@users.noreply.github.com>
2022-01-18 19:45:08 -0500

    devlop

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,96 @@
+// import { useReducer } from 'react';
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
+
+// function that takes the initial state and an action
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
+    // pass default value of intialState and returns a spread copy of state with a product array
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
+    // if the id matches then we update the purchase qty here
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
+    // remove product from the cart after we filter to check if the product id matches the action id. 
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    // TODO: Add a comment describing what the default case is for
+    // return the state 
+    default:
+      return state;
+  }
+};
+
+export default reducer;
